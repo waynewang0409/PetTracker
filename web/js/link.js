8082d6185a119a259b0ab18a0cf2782945d29a51
vn19274 <64690895+vn19274@users.noreply.github.com>
2020-05-03 14:12:11 +0100

    Add files via upload

---
+++ web/js/link.js
@@ -0,0 +1,15 @@
+function linkPets(){
+	window.location = 'index.html';
+}
+function linkInsert(){
+	window.location = 'insert.html';
+}
+function linkUser(){
+	window.location = 'user.html';
+}
+function linkDetail(){
+	window.location = 'detail.html';
+}
+function linkFAQ(){
+	window.location = 'faq.html';
+}
